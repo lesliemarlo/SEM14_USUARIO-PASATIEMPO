1ca9163b3d2980011bb1687c680fed68a583c014
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2023-11-25 10:14:21 -0500

    ajustes

--- src/main/java/com/empresa/service/UsuarioServiceImpl.java
+++ src/main/java/com/empresa/service/UsuarioServiceImpl.java
@@ -1,18 +1,13 @@
 package com.empresa.service;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.empresa.entidades.Opcion;
-import com.empresa.entidades.Pasatiempo;
 import com.empresa.entidades.Rol;
 import com.empresa.entidades.Usuario;
-import com.empresa.entidades.UsuarioHasPasatiempo;
-import com.empresa.entidades.UsuarioHasPasatiempoPK;
-import com.empresa.repository.UsuarioHasPasatiempoRepository;
 import com.empresa.repository.UsuarioRepository;
 
 
@@ -22,8 +17,6 @@ public class UsuarioServiceImpl implements UsuarioService{
 	@Autowired
 	private UsuarioRepository usuarioRepository;
 	
-	@Autowired
-	private UsuarioHasPasatiempoRepository usuarioHasPasatiempoRepository;
 	
 	@Override
 	public Usuario login(Usuario bean) {
@@ -45,29 +38,10 @@ public class UsuarioServiceImpl implements UsuarioService{
 		return usuarioRepository.findByLogin(login);
 	}
 
-	@Override
-	public UsuarioHasPasatiempo insertaPasatiempo(UsuarioHasPasatiempo obj) {
-		return usuarioHasPasatiempoRepository.save(obj);
-	}
-
-	@Override
-	public void eliminaPasatiempo(UsuarioHasPasatiempo obj) {
-		usuarioHasPasatiempoRepository.delete(obj);
-	}
-
-	@Override
-	public Optional<UsuarioHasPasatiempo> buscaPasatiempo(UsuarioHasPasatiempoPK obj) {
-		return usuarioHasPasatiempoRepository.findById(obj);
-	}
-
+	
 	@Override
 	public List<Usuario> listaUsuario() {
 		return usuarioRepository.findAll();
 	}
-
-	@Override
-	public List<Pasatiempo> traerPasatiempoDeUsuario(int idUsuario) {
-		return usuarioRepository.traerPasatiempoDeUsuario(idUsuario);
-	}
-
+	
 }
